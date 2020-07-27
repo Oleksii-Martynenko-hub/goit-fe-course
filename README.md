6fb132cc79a62af4b271bc28497d0f5f247f9f5b
Martynenko <4lexm4rtin@gmail.com>
2020-07-27 23:05:10 +0300

    main page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # goit-fe-course
+# HTML5/CSS3 homeworks at GOIT school
+<a href="https://alexm4rt1n.github.io/goit-fe-course/"><h1>Github pages</h1></a>
---
+++ js/script.js
@@ -0,0 +1,12 @@
+// $(function(event){
+//   $('.homework__item6').mouseover(hover)
+//   $('.homework__item6').mouseout(unhover)
+// });
+// const hover = function() {
+//   $('.homework__item8').css('z-index', '-1');
+//   $('.homework__item10').css('z-index', '-1');
+// };
+// const unhover = function() {
+//   $('.homework__item8').css('z-index', '1');
+//   $('.homework__item10').css('z-index', '1');
+// };
